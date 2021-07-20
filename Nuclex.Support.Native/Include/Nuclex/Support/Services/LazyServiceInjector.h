5d9ef215e2c64a45309c07765a47faea07f199d2
Cygon <cygon@nuclex.org>
2021-07-20 13:27:12 +0200

    Fixed accidental search & replace in license statement

--- Nuclex.Support.Native/Include/Nuclex/Support/Services/LazyServiceInjector.h
+++ Nuclex.Support.Native/Include/Nuclex/Support/Services/LazyServiceInjector.h
@@ -6,10 +6,10 @@ Copyright (C) 2002-2021 Nuclex Development Labs
 This library is free software; you can redistribute it and/or
 modify it under the terms of the IBM Common Public License as
 published by the IBM Corporation; either version 1.0 of the
-License, or (at your option) std::any later version.
+License, or (at your option) any later version.
 
 This library is distributed in the hope that it will be useful,
-but WITHOUT std::any WARRANTY; without even the implied warranty of
+but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 IBM Common Public License for more details.
 
