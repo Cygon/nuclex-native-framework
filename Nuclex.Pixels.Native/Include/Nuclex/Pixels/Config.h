c80cc90a18067f20f1376fd54725d2753762f2e7
Cygon <cygon@nuclex.org>
2021-08-08 14:22:28 +0200

    Updated Config.h needed for Platform API wrappers

--- Nuclex.Pixels.Native/Include/Nuclex/Pixels/Config.h
+++ Nuclex.Pixels.Native/Include/Nuclex/Pixels/Config.h
@@ -33,22 +33,48 @@ License along with this library
 #if defined(WINAPI_FAMILY) && (WINAPI_FAMILY == WINAPI_FAMILY_APP)
   #error The Nuclex.Pixels.Native library does not support WinRT
 #elif defined(WIN32) || defined(_WIN32)
-  #define NUCLEX_PIXELS_WIN32 1
+  #define NUCLEX_PIXELS_WINDOWS 1
 #else
   #define NUCLEX_PIXELS_LINUX 1
 #endif
 
 // --------------------------------------------------------------------------------------------- //
 
-// C++ language features
-#if defined(_MSC_VER) && (_MSC_VER >= 1900) // Visual Studio 2015 has the C++14 features we use
-  #define NUCLEX_PIXELS_CXX14 1
-#elif defined(__clang__) && defined(__cplusplus) && (__cplusplus >= 201402)
-  #define NUCLEX_PIXELS_CXX14 1
-#elif (defined(__GNUC__) || defined(__GNUG__)) && defined(__cplusplus) && (__cplusplus >= 201402)
-  #define NUCLEX_PIXELS_CXX14 1
+// Compiler support checking
+#if defined(_MSC_VER)
+  #if (_MSC_VER < 1910) // Visual Studio 2017 has the C++17 features we use
+    #error At least Visual Studio 2017 is required to compile Nuclex.Pixels.Native
+  #elif defined(_MSVC_LANG) && (_MSVC_LANG >= 201703L)
+    #define NUCLEX_PIXELS_CXX17 1
+  #endif
+#elif defined(__clang__) && defined(__clang_major__)
+  #if (__clang_major__ < 5) // clang 5.0 has the C++17 features we use
+    #error At least clang 5.0 is required to compile Nuclex.Pixels.Native
+  #elif defined(__cplusplus) && (__cplusplus >= 201703L)
+    #define NUCLEX_PIXELS_CXX17 1
+  #endif
+#elif defined(__GNUC__)
+  #if (__GNUC__ < 8) // GCC 8.0 has the C++17 features we use
+    #error At least GCC 8.0 is required to compile Nuclex.Pixels.Native
+  #elif defined(__cplusplus) && (__cplusplus >= 201703L)
+    #define NUCLEX_PIXELS_CXX17 1
+  #endif
 #else
-  #error The Nuclex.Pixels.Native library requires a C++14 compiler
+  #error Unknown compiler. Nuclex.Pixels.Native is tested with GCC, clang and MSVC only
+#endif
+
+// This library uses std::optional, 'if constexpr' and new C++17 containers,
+// so anything earlier than C++ 17 will only result in compilation errors.
+#if !defined(NUCLEX_PIXELS_CXX17)
+  #error The Nuclex.Pixels.Native library must be compiled in at least C++17 mode
+#endif
+
+// We've got tons of u8"hello" strings that will become char8_t in C++20 and fail to build!
+// Bail out instead of letting the user scratch their head over weird compiler errors.
+#if defined(_MSVC_LANG) && (_MSVC_LANG >= 202002)
+  #error The Nuclex.Pixels.Native library does not work in C++20 mode yet
+#elif defined(__cplusplus) && (__cplusplus >= 202002)
+  #error The Nuclex.Pixels.Native library does not work in C++20 mode yet
 #endif
 
 // --------------------------------------------------------------------------------------------- //
@@ -66,9 +92,12 @@ License along with this library
 
 // --------------------------------------------------------------------------------------------- //
 
-//#if defined(__clang__) || (defined(__GNUC__) || defined(__GNUG__))
-//  #define NUCLEX_PIXELS_HAVE_BUILTIN_INT128
-//#endif
+// Enable built-in 128 bit integer type if present
+#if defined(__clang__) || (defined(__GNUC__) || defined(__GNUG__))
+  #if defined(__SIZEOF_INT128__) // Extra check, since GCC __int128 exists only on amd64
+    #define NUCLEX_PIXELS_HAVE_BUILTIN_INT128 1
+  #endif
+#endif
 
 // --------------------------------------------------------------------------------------------- //
 
@@ -76,15 +105,15 @@ License along with this library
 #if defined(_MSC_VER)
   #define NUCLEX_PIXELS_ALWAYS_INLINE __forceinline
 #elif defined(__clang__) || (defined(__GNUC__) || defined(__GNUG__))
-  #define NUCLEX_PIXELS_ALWAYS_INLINE __attribute__((always_inline))
+  #define NUCLEX_PIXELS_ALWAYS_INLINE __attribute__((always_inline)) inline
 #else
-  #define NUCLEX_PIXELS_ALWAYS_INLINE
+  #define NUCLEX_PIXELS_ALWAYS_INLINE inline
 #endif
 
 // --------------------------------------------------------------------------------------------- //
 
 // Decides whether symbols are imported from a dll (client app) or exported to
-// a dll (Nuclex.Storage.Native library). The NUCLEX_PIXELS_SOURCE symbol is defined by
+// a dll (Nuclex.Pixels.Native library). The NUCLEX_PIXELS_SOURCE symbol is defined by
 // all source files of the library, so you don't have to worry about a thing.
 #if defined(_MSC_VER)
 
@@ -99,26 +128,61 @@ License along with this library
       #define NUCLEX_PIXELS_API __declspec(dllimport)
     #endif
   #endif
+  // MSVC doesn't have to import/export the whole type, it includes the vtable
+  // and RTTI stuff automatically when at least one member is exported. Exporting
+  // the whole type would only cause useless things to be exported as well.
+  #define NUCLEX_PIXELS_TYPE
 
-#elif defined(__GNUC__) || defined(__GNUG__) || defined(__clang__)
+#elif defined(__GNUC__) || defined(__clang__)
 
   #if defined(NUCLEX_PIXELS_STATICLIB) || defined(NUCLEX_PIXELS_EXECUTABLE)
     #define NUCLEX_PIXELS_API
+    #define NUCLEX_PIXELS_TYPE
   #else
-    #if defined(NUCLEX_PIXELS_SOURCE)
-      #define NUCLEX_PIXELS_API __attribute__ ((visibility ("default")))
-    #else
-      // If you use -fvisibility=hidden in GCC, exception handling and RTTI would break 
-      // if visibility wasn't set during export _and_ import because GCC would immediately
-      // forget all type infos encountered. See http://gcc.gnu.org/wiki/Visibility
-      #define NUCLEX_PIXELS_API __attribute__ ((visibility ("default")))
-    #endif
+    // If you use -fvisibility=hidden in GCC, exception handling and RTTI would break
+    // if visibility wasn't set during export _and_ import because GCC would immediately
+    // forget all type infos encountered. See http://gcc.gnu.org/wiki/Visibility
+    #define NUCLEX_PIXELS_API __attribute__ ((visibility ("default")))
+    #define NUCLEX_PIXELS_TYPE __attribute__ ((visibility ("default")))
   #endif
 
 #else
 
   #error Unknown compiler, please implement shared library macros for your system
 
+#endif
+
+// --------------------------------------------------------------------------------------------- //
+
+// Optimization macros
+#if defined(__GNUC__) || defined(__clang__)
+
+  #if !defined(likely)
+    #define likely(x) __builtin_expect((x), 1)
+  #endif
+  #if !defined(unlikely)
+    #define unlikely(x) __builtin_expect((x), 0)
+  #endif
+
+#else
+
+  #if !defined(likely)
+    #define likely(x) (x)
+  #endif
+  #if !defined(unlikely)
+    #define unlikely(x) (x)
+  #endif
+
+#endif
+
+// --------------------------------------------------------------------------------------------- //
+
+// Silences unused variable warning, but only in release builds
+// (NDEBUG is also what decides whether the assert() macro does anything)
+#if defined(NDEBUG)
+  #define NUCLEX_PIXELS_NDEBUG_UNUSED(x) (void)x
+#else
+  #define NUCLEX_PIXELS_NDEBUG_UNUSED(x) {}
 #endif
 
 // --------------------------------------------------------------------------------------------- //
